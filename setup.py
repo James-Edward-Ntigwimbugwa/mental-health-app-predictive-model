d50b44ae78b0759187a81aaa1d2d0ca9fd5d851b
James Edward <jamesnavigator04@gmail.com>
2025-06-18 16:46:14 +0300

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a predictive to determine someones level of stress and sickness and provide solutions based on their conditions',
+    author='Juma Maleba',
+    license='MIT',
+)
